85a7d23c165164974b628331adeee920b452e1d0
Russell Kerber <russell.kerber@gmail.com>
2019-12-10 15:53:33 -0600

    bonus logic complete

--- client.js
+++ client.js
@@ -32,12 +32,42 @@ const employees = [
 ];
 
 // YOU SHOULD NOT NEED TO CHANGE ANYTHING ABOVE THIS POINT
+function bonusCalculator(emloyee) {
+  let newEmployee = {
+    name: 'name',
+    bonusPercentage: 'bonusPercentage',
+    totalCompensation: 'totalCompensation',
+    totalBonus: 'totalBonus'
+  };
 
-// Take small steps! Don't write a for loop and two functions that do all of the calculations right away.
-// This problem is massive! Break the problem down. Use the debugger.
-// What is the fewest lines of code I can write and test to get just a little closer?
+  newEmployee.name = employee.name;
 
-// This is not a race. Everyone on your team should understand what is happening.
-// Ask questions when you don't.
+  if (employee.reviewRating <= 2) {
+    newEmployee.bonusPercentage = 0;
+    newEmployee.totalBonus = 0;
+    newEmployee.totalCompensation = employee.annualSalary;
+  }
+  if (employee.reviewRating <= 3) {
+    newEmployee.bonusPercentage = 0.04;
+    newEmployee.totalBonus = .04 * Number(employee.annualSalary);
+    newEmployee.totalCompensation = Number(employee.annualSalary) + (newEmployee.totalBonus);
+  }
+  if (employee.reviewRating <= 4) {
+    newEmployee.bonusPercentage = 0.06;
+    newEmployee.totalBonus = .06 * Number(employee.annualSalary);
+    newEmployee.totalCompensation = Number(employee.annualSalary) + (newEmployee.totalBonus);
+  }
+  if (employee.reviewRating <= 5) {
+    newEmployee.bonusPercentage = 0.1;
+    newEmployee.totalBonus = .1 * Number(employee.annualSalary);
+    newEmployee.totalCompensation = Number(employee.annualSalary) + (newEmployee.totalBonus);
+  }
 
-console.log( employees );
+  // Take small steps! Don't write a for loop and two functions that do all of the calculations right away.
+  // This problem is massive! Break the problem down. Use the debugger.
+  // What is the fewest lines of code I can write and test to get just a little closer?
+
+  // This is not a race. Everyone on your team should understand what is happening.
+  // Ask questions when you don't.
+
+  console.log(employees);
