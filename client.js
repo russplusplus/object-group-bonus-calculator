0c062cb5e42778ef175bf713b14b49df816d116e
Russell Kerber <russell.kerber@gmail.com>
2019-12-11 10:10:50 -0600

    completed function

--- client.js
+++ client.js
@@ -32,7 +32,7 @@ const employees = [
 ];
 
 // YOU SHOULD NOT NEED TO CHANGE ANYTHING ABOVE THIS POINT
-function bonusCalculator(emloyee) {
+function bonusCalculator(employee) {
   let newEmployee = {
     name: 'name',
     bonusPercentage: 'bonusPercentage',
@@ -42,27 +42,61 @@ function bonusCalculator(emloyee) {
 
   newEmployee.name = employee.name;
 
-  if (employee.reviewRating <= 2) {
+  if (employee.reviewRating === 2) {
     newEmployee.bonusPercentage = 0;
-    newEmployee.totalBonus = 0;
-    newEmployee.totalCompensation = employee.annualSalary;
+//    newEmployee.totalBonus = 0;
+//    newEmployee.totalCompensation = employee.annualSalary;
   }
-  if (employee.reviewRating <= 3) {
+  if (employee.reviewRating === 3) {
     newEmployee.bonusPercentage = 0.04;
-    newEmployee.totalBonus = .04 * Number(employee.annualSalary);
-    newEmployee.totalCompensation = Number(employee.annualSalary) + (newEmployee.totalBonus);
+//    newEmployee.totalBonus = .04 * Number(employee.annualSalary);
+//    newEmployee.totalCompensation = Number(employee.annualSalary) + (newEmployee.totalBonus);
   }
-  if (employee.reviewRating <= 4) {
+  if (employee.reviewRating === 4) {
     newEmployee.bonusPercentage = 0.06;
-    newEmployee.totalBonus = .06 * Number(employee.annualSalary);
-    newEmployee.totalCompensation = Number(employee.annualSalary) + (newEmployee.totalBonus);
+//    newEmployee.totalBonus = .06 * Number(employee.annualSalary);
+//    newEmployee.totalCompensation = Number(employee.annualSalary) + (newEmployee.totalBonus);
   }
-  if (employee.reviewRating <= 5) {
-    newEmployee.bonusPercentage = 0.1;
-    newEmployee.totalBonus = .1 * Number(employee.annualSalary);
-    newEmployee.totalCompensation = Number(employee.annualSalary) + (newEmployee.totalBonus);
+  if (employee.reviewRating === 5) {
+    newEmployee.bonusPercentage = 0.10;
+//    newEmployee.totalBonus = .1 * Number(employee.annualSalary);
+//    newEmployee.totalCompensation = Number(employee.annualSalary) + (newEmployee.totalBonus);
+  }
+  if (Number(employee.employeeNumber) <= 9999) {
+    newEmployee.bonusPercentage += 0.05;
   }
 
+  console.log(newEmployee.bonusPercentage);
+  console.log(employee.annualSalary);
+  console.log(newEmployee.totalBonus);
+
+  newEmployee.totalBonus = newEmployee.bonusPercentage * employee.annualSalary;  
+  newEmployee.totalCompensation = Number(employee.annualSalary) + newEmployee.totalBonus;
+
+  if (newEmployee.totalCompensation > 65000){
+    newEmployee.bonusPercentage -= 0.01;
+    newEmployee.totalBonus = newEmployee.bonusPercentage * employee.annualSalary;  
+    newEmployee.totalCompensation = Number(employee.annualSalary) + newEmployee.totalBonus;
+  }
+
+  if (newEmployee.totalBonus > 0.13){
+    newEmployee.totalBonus = 0.13;
+    newEmployee.totalBonus = newEmployee.bonusPercentage * employee.annualSalary;  
+    newEmployee.totalCompensation = Number(employee.annualSalary) + newEmployee.totalBonus;
+  }
+
+  if (newEmployee.totalBonus < 0){
+    newEmployee.totalBonus = 0;
+    newEmployee.totalBonus = newEmployee.bonusPercentage * employee.annualSalary;  
+    newEmployee.totalCompensation = Number(employee.annualSalary) + newEmployee.totalBonus;
+  }
+
+  console.log(newEmployee.bonusPercentage);
+
+  return newEmployee;
+}
+
+console.log(bonusCalculator(employees[0]));
   // Take small steps! Don't write a for loop and two functions that do all of the calculations right away.
   // This problem is massive! Break the problem down. Use the debugger.
   // What is the fewest lines of code I can write and test to get just a little closer?
